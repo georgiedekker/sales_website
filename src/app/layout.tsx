d0868109a1527a960932ffcd10ad848a2263405c
georgiedekker <georgiedekker@gmail.com>
2025-04-02 19:26:03 +0200

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './globals.css';
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'RaiseMyTicket.com | Unified Enterprise Ticket Management',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-VH4MT1CFCE" />
     </html>
   );
 } 
